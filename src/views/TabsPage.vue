ba20a6b8617aca84c00ffdbdf98848495fcb1eb8
YOUR-NAME <googlaram200@gmail.com>
2023-03-25 00:56:57 +0300

    new features

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -4,17 +4,17 @@
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/home">
-          <ion-icon aria-hidden="true" :icon="triangle" />
+          <ion-icon aria-hidden="true" :icon="fileTrayOutline" />
           <ion-label>Операции</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab2" href="/tabs/analytics">
-          <ion-icon aria-hidden="true" :icon="ellipse" />
+          <ion-icon aria-hidden="true" :icon="statsChartOutline" />
           <ion-label>Аналитика</ion-label>
         </ion-tab-button>
 
         <ion-tab-button tab="tab3" href="/tabs/sync">
-          <ion-icon aria-hidden="true" :icon="square" />
+          <ion-icon aria-hidden="true" :icon="cloudyOutline" />
           <ion-label>Синхронизация</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
@@ -24,5 +24,5 @@
 
 <script setup lang="ts">
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { ellipse, square, triangle, statsChartOutline, cloudyOutline, fileTrayOutline } from 'ionicons/icons';
 </script>
